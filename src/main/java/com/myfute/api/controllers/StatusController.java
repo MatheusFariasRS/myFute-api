d9d678f2811463f181c121468d1cd012a3492a29
Matheus Farias <matheusaf.contato@gmail.com>
2026-02-05 17:08:37 -0300

    feat: initial `Spring Boot` app with status endpoint

---
+++ src/main/java/com/myfute/api/controllers/StatusController.java
@@ -0,0 +1,13 @@
+package com.myfute.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String status() {
+        return "MyFute API is up ⚽";
+    }
+}
