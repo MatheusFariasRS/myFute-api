2c8b15a833ba9597d72435bdc43e1fafb314cddd
Matheus Farias <matheusaf.contato@gmail.com>
2026-02-06 12:38:51 -0300

    style: format Java code using project formatter

--- src/main/java/com/myfute/api/controllers/StatusController.java
+++ src/main/java/com/myfute/api/controllers/StatusController.java
@@ -1,13 +1,17 @@
 package com.myfute.api.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
 
-    @GetMapping("/status")
-    public String status() {
-        return "MyFute API is up ⚽";
-    }
+  private static final Logger log = LoggerFactory.getLogger(StatusController.class);
+
+  @GetMapping("/status")
+  public String status() {
+    return "MyFute API is up ⚽";
+  }
 }
