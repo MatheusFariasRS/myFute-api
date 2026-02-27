e36c61b78061b2cf5812d3a9632f8a56a2779ce9
Matheus Farias <matheusaf.contato@gmail.com>
2026-02-27 12:33:51 -0300

    chore(docker): fix `postgres` healthcheck and improve `docker-up` readiness logic

--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-docker compose -f infra/compose.yaml up -d --build
+COMPOSE="docker compose -f infra/compose.yaml"
+
+$COMPOSE up -d --build
+
+CID="$($COMPOSE ps -q database)"
+
+i=0
+while [ "$(docker inspect -f '{{.State.Health.Status}}' "$CID")" != "healthy" ]; do
+  dots=$(printf "%*s" $((i % 4)) "" | tr ' ' '.')
+  printf "\r\033[KWaiting for Postgres (database) to be healthy%s" "$dots"
+  i=$((i + 1))
+  sleep 0.3
+done
+
+printf "\r\033[KPostgres is healthy.\n"
